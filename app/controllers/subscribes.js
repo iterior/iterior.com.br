a9f059082c39ff1261bfb2acd690d1c2a5290708
Matheus <matheus.oliveira@luizalabs.com>
2015-10-19 10:08:18 -0200

    send mail

--- app/controllers/subscribes.js
+++ app/controllers/subscribes.js
@@ -60,6 +60,7 @@ var SubscribesController = {
     });
   },
   send: function (req, res) {
+    var send_mail = require('../mailers/mandrill');
     var email = req.params['email'];
     var query = {};
     if (email) {
@@ -67,16 +68,29 @@ var SubscribesController = {
     }
     Subscribe.find(query, function (err, subscribes) {
       subscribes.forEach(function (err, subscribe) {
-        if (subscribe.email) {
-          var s = new Subscribe({
-            name: subscribe.name,
-            email: subscribe.email,
-            phone: subscribe.phone,
-            address: subscribe.address
-          });
-          s.send_confirmation();
-          s.sended_at = new Date().toISOString();
-          s.save();
+        if (!subscribe.hasOwnProperty('confirmed_at')) {
+          var body = [
+            '<html>',
+            '<head>',
+            '<meta charset="utf-8">',
+            '<title>iTerior Conferece</title>',
+            '</head>',
+            '<body>',
+            'Olá ' + this.name + ', obrigado por se inscrever.',
+            '<p>',
+            'Com vagas limitadas nós realmente estamos contando com a sua presença.',
+            'Caso haja algum imprevisto, por favor <a href="mailto:contato@iterior.com.br">deixe-nos saber</a>.',
+            '</p>',
+            '<p>Mudamos alguns campos de inscrição, por favor atualize <a href="http://iterior.com.br/subscribes/' + this.email + '" target="_blank">seu dados aqui</a>.</p>',
+            '<br>',
+            '--',
+            '<p>Equipe iTerior</p>',
+            '</body>',
+            '</html>'
+          ];
+          send_mail({ name: subscribe.name, email: subscribe.email }, 'Inscrição Confirmada', body.join(''));
+          subscribe.sended_at = new Date().toISOString();
+          subscribe.save();
         }
       });
     });
