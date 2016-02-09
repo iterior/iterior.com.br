5b525dc8b38c3c25b9bd116f8972086b0805dd34
Matheus Oliveira <oliveira.matheusde@gmail.com>
2016-02-08 22:51:22 -0200

    Specify node version

--- app/settings.js
+++ app/settings.js
@@ -2,11 +2,11 @@ const path = require('path');
 const mongoose = require('mongoose');
 const mandrill = require('mandrill-api/mandrill');
 
-SECRET_KEY = 'y0g7#ucnls-vz)x%oybdjw#9m006w!i&hjt#fm=+zkf)&&ceg@';
+exports.SECRET_KEY = 'y0g7#ucnls-vz)x%oybdjw#9m006w!i&hjt#fm=+zkf)&&ceg@';
 
-VIEWS_PATH = path.join(__dirname, 'views');
+exports.VIEWS_PATH = path.join(__dirname, 'views');
 
-ROUTES_ROOT = path.join(__dirname, '/routes');
+exports.ROUTES_ROOT = path.join(__dirname, 'routes');
 
 exports.db = mongoose.connect(process.env.MONGOLAB_HOST);
 
